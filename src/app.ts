08cc1b70dabda8e98162c29a80e04fd6de11724e
Bravoezz <joelmrd1234@gmail.com>
2023-09-04 21:33:24 -0500

    init commit

---
+++ src/app.ts
@@ -0,0 +1,29 @@
+import express, { Response } from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+// loguar peticiones
+app.use(morgan('dev'));
+
+// configurar los cors
+app.use(cors());
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/* Configurar las rutas */
+
+app.get('/', (_, res: Response) => {
+    res.status(200).send('hola ');
+});
+
+app.listen(port, () => {
+    console.log('server ready in ' + port);
+});
+
+export default app;
